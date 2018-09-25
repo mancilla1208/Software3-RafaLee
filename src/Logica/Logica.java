33138deb3020fca33d6b24a24e9b0041a6f67d41
FelipeO <Familia MO@192.168.1.60>
2018-09-25 16:57:40 -0500

    Se agrego validación en el momento que un estudiante selccionae el nombre

---
+++ src/Logica/Logica.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author andre
+ */
+public class Logica {
+    
+    public void imprimir(){
+        System.out.println("hola");
+        
+    }
+    
+}
