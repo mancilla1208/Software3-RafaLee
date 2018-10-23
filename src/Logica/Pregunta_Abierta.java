ae976d53be59486c91d769a6a4c86026328853ea
Andres Mancilla <admancillao@uqvirtual.edu.co>
2018-10-22 23:31:13 -0500

    Ingreso de clases para el manejo de preguntas y sus respectivas respuestas.

---
+++ src/Logica/Pregunta_Abierta.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author andre
+ */
+public class Pregunta_Abierta {
+
+    private String respuesta;
+
+    public Pregunta_Abierta(String respuesta) {
+        super();
+        this.respuesta = respuesta;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+}
---
+++ src/Logica/Pregunta_Icfes.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author andre
+ */
+public class Pregunta_Icfes {
+
+    private String res_opcion_A;
+    private String res_opcion_B;
+    private String res_opcion_C;
+    private String res_opcion_D;
+
+    public Pregunta_Icfes(String res_opcion_A, String res_opcion_B, String res_opcion_C, String res_opcion_D) {
+        super();
+        this.res_opcion_A = res_opcion_A;
+        this.res_opcion_B = res_opcion_B;
+        this.res_opcion_C = res_opcion_C;
+        this.res_opcion_D = res_opcion_D;
+    }
+
+    public String getRes_opcion_A() {
+        return res_opcion_A;
+    }
+
+    public void setRes_opcion_A(String res_opcion_A) {
+        this.res_opcion_A = res_opcion_A;
+    }
+
+    public String getRes_opcion_B() {
+        return res_opcion_B;
+    }
+
+    public void setRes_opcion_B(String res_opcion_B) {
+        this.res_opcion_B = res_opcion_B;
+    }
+
+    public String getRes_opcion_C() {
+        return res_opcion_C;
+    }
+
+    public void setRes_opcion_C(String res_opcion_C) {
+        this.res_opcion_C = res_opcion_C;
+    }
+
+    public String getRes_opcion_D() {
+        return res_opcion_D;
+    }
+
+    public void setRes_opcion_D(String res_opcion_D) {
+        this.res_opcion_D = res_opcion_D;
+    }
+
+}
