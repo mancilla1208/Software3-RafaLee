917b9e93e461d4666d3bacbc27705a78e708c146
Andres Mancilla <admancillao@uqvirtual.edu.co>
2019-02-12 17:00:58 -0500

    Se agrego manual de ayuda para los estudiantes.

--- src/GUI/Estudiante.java
+++ src/GUI/Estudiante.java
@@ -167,7 +167,7 @@ public class Estudiante extends javax.swing.JInternalFrame {
                 jButtonSalirEstudianteActionPerformed(evt);
             }
         });
-        jPanel2.add(jButtonSalirEstudiante, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 20, 62, 45));
+        jPanel2.add(jButtonSalirEstudiante, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 20, 62, 45));
 
         jButtonVolverEstu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/volver2.png"))); // NOI18N
         jButtonVolverEstu.setContentAreaFilled(false);
